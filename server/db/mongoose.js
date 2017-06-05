6855715d592cbb72fb5d1440afb651b2f3991a84
antonmyr <15anmy@stockholmscience.se>
2017-06-05 13:15:52 +0200

    Setup app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/DevCafes');
+mongoose.connect(
+	process.env.MONGODB_URI || 'mongodb://localhost:27017/DevCafes'
+);
 
 module.exports = {
 	mongoose
--- server/server.js
+++ server/server.js
@@ -1,29 +1,29 @@
-const express = require("express");
-const bodyParser = require("body-parser");
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const indexRoute = require("./routes/");
+const indexRoute = require('./routes/');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-const ENV = process.NODE_ENV || "development";
+const ENV = process.NODE_ENV || 'development';
 
-if (ENV === "development") {
-  console.log("Dev");
-  app.use(require("morgan")("dev"));
-  // app.use(errorHandler({ dumpExceptions: true, showStack: true }));
-} else if (ENV === "production") {
-  console.log("Prod");
-  // app.use(errorHandler());
+if (ENV === 'development') {
+	console.log('Dev');
+	app.use(require('morgan')('dev'));
+	// app.use(errorHandler({ dumpExceptions: true, showStack: true }));
+} else if (ENV === 'production') {
+	console.log('Prod');
+	// app.use(errorHandler());
 }
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use("/", indexRoute);
+app.use('/', indexRoute);
 
 app.listen(port, () => {
-  console.log(`Server started on port ${port}`);
+	console.log(`Server started on port ${port}`);
 });
 
 module.exports = { app };
