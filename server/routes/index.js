a44205c29940f6f7dc7857804be0be8a56529561
antonmyr <15anmy@stockholmscience.se>
2017-06-05 15:07:16 +0200

    fixed unmerged file routes/index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,19 +1,10 @@
-const express = require('express');
-<<<<<<< HEAD
-
-const router = require(express.Router());
-=======
 const { mongoose } = require('../db/mongoose');
 
 const router = express.Router();
->>>>>>> 9958e767e0769d54ce7cffe8a3a60b907b3e4c05
 
 //Controllers
 const VenueController = require('../controllers/VenueController');
 
-<<<<<<< HEAD
-router.route('/venues').post();
-=======
 router
 	.route('/venues')
 	.post(VenueController.addVenue)
@@ -21,4 +12,3 @@ router
 
 router.route('/venue/:id').delete(VenueController.removeVenue);
 module.exports = router;
->>>>>>> 9958e767e0769d54ce7cffe8a3a60b907b3e4c05
